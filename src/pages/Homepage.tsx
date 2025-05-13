727dad9fba7a4417614f2f9911ce93a97e931a7e
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-05-13 20:00:44 -0300

    adicionado Speed Test a tela inicial

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -10,16 +10,15 @@ function HomePage(){
             <Header></Header>
         </header>
         <div className="container">
-            <div className="texto-analise">
-                <p> Este projeto tem como objetivo o desenvolvimento de uma aplicação web interativa, com um frontend intuitivo, que permitirá aos usuários analisar a qualidade da rede Wi-Fi em seus ambientes.</p>
-                <p> A aplicação irá identificar problemas como níveis de sinal insuficientes, baixa velocidade e interferências, proporcionando uma análise detalhada e soluções possíveis para cada cômodo da residência ou estabelecimento. </p>
-            </div>
-            <div className="imagens">
-                <img src="../src/img/wifi-topo.png" alt="rede wifi e usuários conectados" />
-                <img src="../src/img/image.webp" alt="grafico de crescimento." />
-            </div>
+         <iframe className="speedTest"
+        src="https://speed.cloudflare.com/"
+        width="100%"
+        height="420"
+        frameBorder="0"
+        ></iframe>
         </div>
-        <CustomButton label="Iniciar Análise" to="/MyForms"/>
+        <a href="../MyForms" className="homeButton"><CustomButton label="Iniciar Análise"/></a>
+        
         </>
     )
 }
