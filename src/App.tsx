ecc04e289372495c02662ba11d421053db15a709
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-05-19 15:12:50 -0300

    fix: delete the iframe the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyForms from './pages/MyForms';
 import Graficos from './pages/Graficos';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from './pages/Homepage';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-           <Route path='/' element={<HomePage/>}/>
-          <Route path='/MyForms' element={<MyForms/>}/>
+          <Route path='/' element={<MyForms/>}/>
           <Route path='/graficos' element={<Graficos />} />
         </Routes>
       </Router>
